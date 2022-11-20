9301daeaaf9a19dd960e61bb37cd45c25ddbb580
Jesus Mata <alejandroavila98@hotmail.com>
2022-11-19 23:32:40 -0600

    Added LoginRequest, RegisterRequest, RegisterResponse, Added Auth Docs

---
+++ MyHistory.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyHistory.Contracts.Authentication;
+
+namespace MyHistory.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [Route("register")]
+        public IActionResult Register(RegisterRequest request )
+        {
+            return Ok(request);
+        }
+
+        [Route("login")]
+        public IActionResult Login (LoginRequest request)
+        {
+            return Ok(request);
+        }
+    }
+
+}
