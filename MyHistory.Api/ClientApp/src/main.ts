f2ddfb4a04cd4ba5a9818f4387b7ed1a2b6d8aaf
viex <carp_97@outlook.com>
2023-01-03 15:35:39 -0600

    - Added vuetify. - Separated app modules into their own folders.

--- MyHistory.Api/ClientApp/src/main.ts
+++ MyHistory.Api/ClientApp/src/main.ts
@@ -1,14 +1,8 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from "./App.vue";
+import "./assets/main.css";
+import { createApp } from "vue";
+import { pinia } from "./pinia";
+import { router } from "./router";
+import { vuetify } from "./vuetify";
 
-import App from './App.vue'
-import router from './router'
-
-import './assets/main.css'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(pinia).use(router).use(vuetify).mount("#app");
